26137bf27d8f54e52f6f017b28cb156cac290d6b
MaestraMatta <47540789+MaestraMatta@users.noreply.github.com>
2022-09-28 23:26:38 +0200

    File iniziali per il progetto MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Matilde")
+    basic.showIcon(IconNames.Heart)
+    basic.pause(2000)
+    basic.clearScreen()
+})
+basic.forever(function () {
+	
+})
